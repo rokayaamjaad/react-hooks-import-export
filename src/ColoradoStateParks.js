bac3915d52158d6a8f5ffbbee17161046c01ccf6
rokayaamjaad <Rukayaamged733@gmail.com>
2022-12-10 00:10:43 +0300

    finsh

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,12 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
 
-function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+export default function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerdePark />
+    </div>
+  );
 }
